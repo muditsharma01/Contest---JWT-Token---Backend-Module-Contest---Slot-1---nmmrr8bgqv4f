369a420d3b4e9e57a6c829ef9034908fc2a196e3
Mudit Sharma <muditshar001@gmail.com>
2023-11-21 18:04:07 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,7 @@
-//Implement loginUser Function Below 
-
 const User = require('../models/userModel');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
-const secretKey='newtonSchoolContest'
+const secretKey = 'newtonSchoolContest';
 
 const registerUser = async (req, res) => {
   try {
@@ -25,17 +23,30 @@ const registerUser = async (req, res) => {
   }
 };
 
-// TODO: Implement user login logic
 const loginUser = async (req, res) => {
   const { username, password } = req.body;
 
-try {
-  //Write a code here for login of a usename and generate a JWT token and send that as a json response
-  //Remember to check a password that has been hashed using bcrypt
-} catch (error) {
-  console.error(error);
-  res.status(500).json({ error: 'Internal server error' });
-}
+  try {
+    // Find the user in the database by username
+    const user = await User.findOne({ username });
+
+    // Check if the user exists and if the provided password is correct
+    if (user && (await bcrypt.compare(password, user.password))) {
+      // Generate JWT token with user information
+      const token = jwt.sign({ username: user.username }, secretKey, {
+        expiresIn: '1h', // Token expiration time (e.g., 1 hour)
+      });
+
+      // Send the token as a JSON response
+      res.status(200).json({ token });
+    } else {
+      // Invalid username or password
+      res.status(401).json({ error: 'Invalid username or password' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 };
 
 module.exports = { registerUser, loginUser };
